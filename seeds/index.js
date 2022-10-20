2dda6e67088702661560225fbedd22de66365dd7
alotofsnakes <mrfries@gmail.com>
2022-10-19 21:54:46 -0700

    added seeds

---
+++ seeds/index.js
@@ -0,0 +1,14 @@
+const sequelize = require('../config/connection');
+const seedUser= require('./userData');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedUser();
+  console.log('---Users Seeded---');
+
+
+  process.exit(0);
+};
+
+seedAll();
