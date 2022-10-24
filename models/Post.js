1e9951ba2c991eb092709fd15711b296ee89f1b8
alotofsnakes <mrfries@gmail.com>
2022-10-23 20:38:21 -0700

    reverted back to post_url

--- models/Post.js
+++ models/Post.js
@@ -15,7 +15,7 @@ Post.init(
         type: DataTypes.STRING,
         allowNull: false
       },
-      post_content: {
+      post_url: {
         type: DataTypes.STRING,
         allowNull: false,
       },
